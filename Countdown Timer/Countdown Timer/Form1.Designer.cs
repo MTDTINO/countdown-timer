235741f88649ab78d308a749052059c539f60fcf
MTDTINO <103025712+MTDTINO@users.noreply.github.com>
2023-09-20 10:32:14 +0200

    Update Form1.Designer.cs

--- Countdown Timer/Countdown Timer/Form1.Designer.cs
+++ Countdown Timer/Countdown Timer/Form1.Designer.cs
@@ -30,12 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.hoursLabel = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.stopButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.hourTextbox = new System.Windows.Forms.TextBox();
             this.resetButton = new System.Windows.Forms.Button();
+            this.minuteTextbox = new System.Windows.Forms.TextBox();
+            this.secondTextbox = new System.Windows.Forms.TextBox();
+            this.minutesLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer
@@ -43,15 +47,15 @@
             this.timer.Interval = 1000;
             this.timer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label1
+            // hoursLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(146, 85);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "HH:MM:SS";
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursLabel.Location = new System.Drawing.Point(125, 85);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(54, 31);
+            this.hoursLabel.TabIndex = 0;
+            this.hoursLabel.Text = "HH";
             // 
             // startButton
             // 
@@ -83,13 +87,13 @@
             this.stopButton.UseVisualStyleBackColor = true;
             this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
             // 
-            // textBox1
+            // hourTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 62);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(174, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hourTextbox.Location = new System.Drawing.Point(133, 62);
+            this.hourTextbox.Name = "hourTextbox";
+            this.hourTextbox.Size = new System.Drawing.Size(39, 20);
+            this.hourTextbox.TabIndex = 3;
+            this.hourTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // resetButton
             // 
@@ -101,19 +105,60 @@
             this.resetButton.UseVisualStyleBackColor = true;
             this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
+            // minuteTextbox
+            // 
+            this.minuteTextbox.Location = new System.Drawing.Point(193, 62);
+            this.minuteTextbox.Name = "minuteTextbox";
+            this.minuteTextbox.Size = new System.Drawing.Size(39, 20);
+            this.minuteTextbox.TabIndex = 3;
+            this.minuteTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // secondTextbox
+            // 
+            this.secondTextbox.Location = new System.Drawing.Point(255, 62);
+            this.secondTextbox.Name = "secondTextbox";
+            this.secondTextbox.Size = new System.Drawing.Size(41, 20);
+            this.secondTextbox.TabIndex = 3;
+            this.secondTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // minutesLabel
+            // 
+            this.minutesLabel.AutoSize = true;
+            this.minutesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesLabel.Location = new System.Drawing.Point(185, 85);
+            this.minutesLabel.Name = "minutesLabel";
+            this.minutesLabel.Size = new System.Drawing.Size(58, 31);
+            this.minutesLabel.TabIndex = 0;
+            this.minutesLabel.Text = "MM";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.Location = new System.Drawing.Point(251, 85);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(50, 31);
+            this.secondsLabel.TabIndex = 0;
+            this.secondsLabel.Text = "SS";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 327);
             this.Controls.Add(this.resetButton);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.secondTextbox);
+            this.Controls.Add(this.minuteTextbox);
+            this.Controls.Add(this.hourTextbox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.stopButton);
             this.Controls.Add(this.startButton);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.minutesLabel);
+            this.Controls.Add(this.hoursLabel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,12 +167,16 @@
         #endregion
 
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label hoursLabel;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button stopButton;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox hourTextbox;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TextBox minuteTextbox;
+        private System.Windows.Forms.TextBox secondTextbox;
+        private System.Windows.Forms.Label minutesLabel;
+        private System.Windows.Forms.Label secondsLabel;
     }
 }
 
